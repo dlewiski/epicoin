4a5c2d1cd1598a6b27f84a7c7b57995d11fdf13a
David Lewis and Jared Clemmensen <david+jared@>
2018-03-06 11:58:02 -0800

    add methods to transfer class to sign a transaction

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,28 @@
+require 'digest'
+require 'peer'
+
 class Transfer < ActiveRecord::Base
   belongs_to :sender, :class_name => "Peer"
   belongs_to :recipient, :class_name => "Peer"
   belongs_to :block
+  before_create(:sign)
+
+  def message
+    Digest::SHA256.hexdigest([self.sender_id, self.recipient_id, self.amount].join)
+  end
+
+  private
+
+  def sign
+    peer_id = self.sender_id
+    peer_send = Peer.find(peer_id.to_i)
+    priv_key = peer_send.private_key
+    peer_send.sign(self.message, priv_key)
+  end
 
 end
+
+
 
 # belongs to two peers
 # belongs to block
