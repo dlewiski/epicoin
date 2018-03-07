475783638cd9315e256125da4309a18323c3478b
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-07 11:31:20 -0800

    implement block transfer validation before create

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -5,13 +5,24 @@ describe 'Block' do
     it 'creates a new block' do
       sender = Peer.create({:name => 'David', :balance => 1000})
       recipient = Peer.create({:name => 'Jared', :balance => 0})
-      transfer = Transfer.create({:amount => 1000, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
-      new_block = Block.create({:transfer_id => transfer.id})
+      new_transfer = Transfer.create({:amount => 1000, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
+      new_block = Block.create({:transfer_id => new_transfer.id})
+      new_block.transfer = new_transfer
       expect(new_block.nonce).to_not eq(nil)
-      expect(new_block.hash).to_not eq(nil)
+      expect(new_block.own_hash).to_not eq(nil)
       hash_check = Digest::SHA256.hexdigest([new_block.message, new_block.prev_hash, new_block.nonce].compact.join)
       expect(hash_check.start_with?('0000')).to eq(true)
-      expect(transfer.is_valid).to eq(true)
+      expect(new_transfer.is_valid).to eq(true)
+    end
+
+    it 'wont mine if transfer isnt valid' do
+      sender = Peer.create({:name => 'David', :balance => 100})
+      recipient = Peer.create({:name => 'Jared', :balance => 0})
+      transfer = Transfer.create({:amount => 1000, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
+      new_block = Block.create({:transfer_id => transfer.id})
+      expect(Block.all).to eq([])
+      expect(new_block.nonce).to eq(nil)
+      expect(new_block.own_hash).to eq(nil)
     end
   end
 end
