bac62468658ba78945eb678cf1aa03b583706212
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-07 10:26:36 -0800

    add transfer amount validations

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Block' do
   describe 'self.mine' do
     it 'creates a new block' do
-      sender = Peer.create({:name => 'David', :balance => 0})
+      sender = Peer.create({:name => 'David', :balance => 1000})
       recipient = Peer.create({:name => 'Jared', :balance => 0})
       transfer = Transfer.create({:amount => 1000, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
       new_block = Block.create({:transfer_id => transfer.id})
@@ -11,6 +11,7 @@ describe 'Block' do
       expect(new_block.hash).to_not eq(nil)
       hash_check = Digest::SHA256.hexdigest([new_block.message, new_block.prev_hash, new_block.nonce].compact.join)
       expect(hash_check.start_with?('0000')).to eq(true)
+      expect(transfer.is_valid).to eq(true)
     end
   end
 end
--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -22,9 +22,20 @@ describe 'Transfer' do
     expect(sender.balance).to eq(1000)
   end
 
-  # it 'validates transfer before updating balances' do
-  #   sender = Peer.create({:name => 'David', :balance => 1000})
-  #   recipient = Peer.create({:name => 'Jared', :balance => 0})
-  #
-  # end
+  it 'doesnt allow spending below 0' do
+    sender = Peer.create({:name => 'Mike', :balance => 0})
+    recipient = Peer.create({:name => 'Jared', :balance => 1000})
+    transfer = Transfer.create({:amount => 500, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
+    expect(transfer.is_valid).to eq(false)
+    expect(sender.balance).to eq(0)
+    expect(recipient.balance).to eq(1000)
+  end
+
+  it 'doesnt allow negative amounts' do
+    sender = Peer.create({:name => 'Mike', :balance => 10})
+    recipient = Peer.create({:name => 'Jared', :balance => 0})
+    transfer = Transfer.create({:amount => -5, :sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key})
+    expect(sender.balance).to eq(10)
+    expect(recipient.balance).to eq(0)
+  end
 end
