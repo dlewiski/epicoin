761c2022ec32498c8ec1c8914e682e5ed19c9c88
David Lewis and Jared Clemmensen <jared@>
2018-03-08 11:28:54 -0800

    add display for transfers that have yet to be mined

--- epicoin.rb
+++ epicoin.rb
@@ -6,13 +6,13 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
-  Block.create({:transfer_id => nil})
   @blocks = Block.all
   @peers = Peer.all
   @left_position = 0
   @top_position = 150
   @transfers = Transfer.all
-  erb:index
+  @transfers_tobe_mined = Transfer.where({:block_id => nil, :is_valid => true})
+  erb:example_behaviors
 end
 
 post('/new_peer') do
@@ -29,14 +29,14 @@ post('/transaction') do
   sender = Peer.where("public_key LIKE ?", "%#{params[:sender_public]}%").first
   recipient = Peer.where("public_key LIKE ?", "%#{params[:recipient_public]}%").first
   amount = params[:amount].to_i
-  binding.pry
   new_transfer = Transfer.create({:sender_id => sender.id, :recipient_id => recipient.id, :sender_private => sender.private_key, :amount => amount})
   redirect to '/'
 end
 
 post('/mine') do
+  transfer = Transfer.where({:block_id => nil, :is_valid => true}).first
   miner = Peer.where("public_key LIKE ?", "%#{params[:miner_key]}%").first
-  mine_transfer = Transfer.find(params[:transfer_id].to_i)
-  new_block = Block.create({:transfer_id => mine_transfer.id, :miner_id => miner.id})
+  # mine_transfer = Transfer.find(params[:transfer_id].to_i)
+  new_block = Block.create({:miner_id => miner.id})
   redirect to '/'
 end
